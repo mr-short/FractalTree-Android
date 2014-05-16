510341a8b1214a25e828bfcd6fc5313dc0863864
Michael Short <EitrGin@gmail.com>
2014-05-16 16:29:37 -0500

    Refactoring

--- FractalTree/desktop/src/net/eitr/fractaltree/desktop/DesktopLauncher.java
+++ FractalTree/desktop/src/net/eitr/fractaltree/desktop/DesktopLauncher.java
@@ -1,12 +1,16 @@
 package net.eitr.fractaltree.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import net.eitr.fractaltree.FractalTree;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher 
+{
+	public static void main (String[] arg) 
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = true;
 		config.width = 1600;
 		config.height = 900;
 		new LwjglApplication(new FractalTree(), config);
